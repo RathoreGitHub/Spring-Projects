a525ab43049d54135bb0b106b906e20ad230fd9c
RathoreGitHub <37141825+RathoreGitHub@users.noreply.github.com>
2020-04-04 19:57:55 +0530

    Add files via upload
    Banking APP

---
+++ banking-service/banking-service/src/test/java/com/deloitte/banking/BankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deloitte.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
